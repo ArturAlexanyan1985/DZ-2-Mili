eb34d6bb5d559ab3814dc78b0e1f3925564142ca
ArturAlexanyan1985 <93946175+ArturAlexanyan1985@users.noreply.github.com>
2022-07-17 13:01:53 +0300

    Update and rename src/Main.java to источник/Main.java

---
+++ источник/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        int bilet = 100;
+
+        int miles = bilet / 20;
+
+        System.out.println("bonus: " + miles);
+    }
+
+}
